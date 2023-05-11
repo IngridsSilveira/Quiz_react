9b3e73576377cc060e1f5f8485e18b9098212b3e
IngridsSilveira <ingridsouzaok@gmail.com>
2023-05-11 10:36:22 -0300

    questions add

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import './App.scss';
+
+import { useContext } from 'react';
+import { QuizContext } from './context/quiz';
+
 import Welcome from './componentes/Welcome';
+import Question from './componentes/Question'
+import { useEffect } from 'react';
 
 function App() {
+  const [quizState, dispatch] = useContext(QuizContext)
+
+  useEffect(() => {
+    dispatch({type: 'REORDER_QUESTIONS'})
+  }, [])
+
+
   return (
     <div className="App">
       <h1>Quiz de Programação</h1>
-      <Welcome/>
+      {quizState.gameStage === 'Start' && <Welcome/>}
+      {quizState.gameStage === 'Playing' && <Question/>}
+
     </div>
   );
 }
