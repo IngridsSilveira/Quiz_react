e5e2db867494db784de6afa5ca86a9d0743b9466
IngridsSilveira <ingridsouzaok@gmail.com>
2023-05-12 10:54:23 -0300

    options add

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
 
   useEffect(() => {
     dispatch({type: 'REORDER_QUESTIONS'})
-  }, [])
+  }, [dispatch])
 
 
   return (
--- src/componentes/GameOver.jsx
+++ src/componentes/GameOver.jsx
@@ -1,9 +1,19 @@
 import './GameOver.scss'
 
+import { useContext } from 'react'
+import { QuizContext } from '../context/quiz'
+
+import WellDone from '../img/welldone.svg'
+
 const GameOver = () => {
+  const [quizState, dispatch] = useContext(QuizContext)
   return (
-    <div>
-        <h2>Game Over</h2>
+    <div id='gameover'>
+        <h2>Fim de jogo!</h2>
+        <p>Pontuação: {quizState.score}</p>
+        <p>Você acertou {quizState.score} de {quizState.questions.length}{" "} perguntas.</p>
+        <img src={WellDone} alt="fim do quiz" />
+        <button onClick={() => dispatch({type: "NEW_GAME"})}>Reiniciar</button>
     </div>
   )
 }
